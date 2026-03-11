61972a5fadd1d7039a2e89eca204922597450fee
ACHAL RAJ <rajachal717@gmail.com>
2026-03-11 19:13:56 +0530

    Successfully implemented API call with requests library

--- src/main.py
+++ src/main.py
@@ -1,9 +1,3 @@
-def main():
-    name = input("What is your name? ")
-    print(f"Hello, {name}! Your Python environment is officially working.")
-
-if __name__ == "__main__":
-    main()
 import requests
 
 def get_space_info():
